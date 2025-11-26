545742e58235b1d26a5d1367757dcc74995d6512
thekitsunedev <kisbendeguz2007@gmail.com>
2025-11-27 00:29:07 +0100

    Partial commit: Editor

--- data/uiprototype/init.py
+++ data/uiprototype/init.py
@@ -1,14 +1,16 @@
 import pygame
 from data.uiprototype import *
 from data.uiprototype.navigator import *
+from data.uiprototype.editor import Editor
 
 def init():
     pygame.init()
+    ctx = Context()
     scenes = {
         "MainMenu": MenuScene("scene1"),
-        "Collection": CollectionScene("scene2")
+        "Collection": CollectionScene("scene2"),
+        "WorldEditor": Editor("editor")
     }
-    ctx = Context()
     nav = Navigator(ctx, scenes)
     nav.start("MainMenu")
 
