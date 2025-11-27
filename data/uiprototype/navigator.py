4c8ddf43b629a6750345a2b9a8d2e83087539061
menyhardnandor <menyhardnandor@gmail.com>
2025-11-27 18:42:53 +0100

    Created MainMenu, StarterMenu

--- data/uiprototype/navigator.py
+++ data/uiprototype/navigator.py
@@ -41,65 +41,37 @@ class MenuScene(Scene):
         bg = (127,127,127)
         ctx.screen.fill(bg)
         msg = ""
+        def fight():
+            print("Harc")
+        def collection():
+            nav.navigate("Collection")
+        def quit():
+            nav.navigate("QUIT")
+        
+        menu = pygame_menu.Menu(
+            title="",
+            width=1920,
+            height=1080,
+            theme=pygame_menu.themes.THEME_DARK,
+
+        )
+
+        menu.add.button("Harc", fight)
+        menu.add.button("Gyűjtemény", collection)
+        menu.add.button("Kilépés",quit)
+        
+        
         while nav.running:
-            #setup
-            events = pygame.event.get()
-
-            # rajz
             ctx.screen.fill(bg)
-
-            menu_items = ["Harc", "Gyűjtemény", "Kazamaták", "Beállítások", "Világ szerkesztő", "Kilépés"]
-            buttons = []
-
-            y_start = 100
-            vertical_spacing = 150
-            mouse_pos = pygame.mouse.get_pos()
-
-            for i, text_str in enumerate(menu_items):
-
-                
-                text_color = (255, 255, 255)
-                bg_color = (50, 50, 50)       
-            
-                
-                text = ctx.font.render(text_str, True, text_color)
-                rect = text.get_rect()
-                rect.centerx = ctx.screen.get_width() // 2
-                rect.y = y_start + i * vertical_spacing
-
-                
-                box_rect = pygame.Rect(
-                    rect.x - 10, rect.y - 5,
-                    rect.width + 20, rect.height + 10
-                )
-
-                # Hover effekt
-                if box_rect.collidepoint(mouse_pos):
-                    text_color = (200, 200, 200)     
-                    bg_color = (100, 100, 100)       
-                    border_color = (200, 200, 200)   
-                    text = ctx.font.render(text_str, True, text_color)
-
-                # rajzolás
-                pygame.draw.rect(ctx.screen, bg_color, box_rect)         
-                pygame.draw.rect(ctx.screen, bg, box_rect, 2)  
-                ctx.screen.blit(text, rect)                              
-
-                buttons.append((box_rect, text_str))
-
-
-            #event
-            if events:
-                for event in events:
-                    if event.type == pygame.QUIT:
-                        nav.navigate("QUIT")
-                        return
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if rect.collidepoint(event.pos):
-                            ## DEBUG
-                            nav.navigate("Collection")
-                            #nav.navigate("Collection")
-                            return
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    nav.navigate("QUIT")
+                    return
+            if menu.is_enabled():
+                menu.update(events)
+                menu.draw(ctx.screen)
+            pygame.display.update()
                 
 
             #update
